b0627f2d66cc0fc73ea4715439d05c6c5af48764
unknown <j_cho6@fanshaweonline.ca>
2019-04-11 02:24:06 -0400

    stylized, transition added

--- public/js/modules/ChatMessage.js
+++ public/js/modules/ChatMessage.js
@@ -7,7 +7,22 @@ export default {
             {{msg.message.content}}
         </p>
     `,
-
+    mounted(){
+        var body = document.querySelector("body");
+        var p1s = document.querySelectorAll(".new-message");
+        var count = 0;
+        var waypoint = new Waypoint({
+            element: body,
+            handler: function(direction) {
+            for (var i=0;i<p1s.length;i++){
+                count = i;
+            }
+            TweenMax.from(p1s[count], 1, {y:100,opacity:0, ease:Power2.easeOut});
+                this.destroy();
+            },
+            offset: 0
+          });
+    },
     data: function() {
         return {
             matchedID: this.$parent.socketID == this.msg.id
